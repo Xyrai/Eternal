9bdc06ee745b11aedfa8b32526453a7a748782c7
Xyrai <Xyraidev@gmail.com>
2021-03-22 00:21:57 +0100

    Added better API routing handler

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
-from routes import router
+from routers.user import router
+from fastapi.responses import HTMLResponse
 
 app = FastAPI(title="Eternal", description="Eternal FastAPI")
 app.include_router(router, prefix="/api")
+
+
+@app.get("/api", response_class=HTMLResponse)
+async def root():
+    return f"<body><h1>Eternal API made by Kinesey</h1></body>"
--- src/backend/routers/user.py
+++ src/backend/routers/user.py
@@ -2,23 +2,22 @@ import os
 
 from dotenv import load_dotenv, find_dotenv
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import HTMLResponse
 
 from lib.erbs_api import ERBSApi
 
 load_dotenv(find_dotenv())
 
 api_key = os.environ['ERBSKey']
-router = APIRouter()
 ERBS_api = ERBSApi(api_key)
 
+router = APIRouter(
+    prefix="/user",
+    tags=["user"],
+    responses={404: {"description": "Not found"}},
+)
 
-@router.get("/", response_class=HTMLResponse)
-def home():
-    return f"<body><h1>Eternal API made by Kinesey</h1></body>"
 
-
-@router.get("/user/nickname")
+@router.get("/nickname")
 def get_user_number(nickname: str):
     try:
         user_num = ERBS_api.get_user_num_by_nickname(nickname)
